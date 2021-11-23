5a495885b14685ff0260174dcb1319410737a259
Primo Acho <primo.acho@BOL-LT-210913G.local>
2021-11-23 10:36:39 -0400

    node-advanced project added

---
+++ node-advanced/advNode/tests/blogs.test.js
@@ -0,0 +1,17 @@
+const Page = require('./helpers/page');
+
+let page;
+beforeEach(async () => {
+    page = await Page.build();
+    await page.goto('http://localhost:3000');
+});
+
+afterEach(async ()=> {
+    await page.close();
+});
+
+
+TextDecoderStream('whwn loggi, can se blog ', async ()=>{
+    await page.login();
+    await page.goto('http://localhost:3000/blogs')
+});
